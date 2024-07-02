592a31c841deb1840477b5676edd2f90d9c9c438
MartinLFD <168610198+MartinLFD@users.noreply.github.com>
2024-07-01 20:42:16 -0400

    FirstChanges

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+
+let pica = ["♦","♥","♠","♣"];
+let numbers = ["2","3","4","5","6","7","8","9","10","J","Q","K"];
+
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  RandomCardGenerator();
+}
+
+
+function RandomCardGenerator() {
+  
+  const picaGenerator = pica[Math.floor(Math.random() * pica.length)];
+  const NumberGenerator = numbers[Math.floor(Math.random() * numbers.length)];
+
+  document.querySelector("#UpPica").innerHTML = picaGenerator;
+  document.querySelector("#NumberCenter").innerHTML = NumberGenerator;
+  document.querySelector("#BottomPica").innerHTML = picaGenerator;
+
+  if(picaGenerator === "♦" || picaGenerator === "♥"){
+      document.querySelector("#UpPica").style.color = "red";
+      document.querySelector("#BottomPica").style.color = "red";
+  }
+
+}
+
